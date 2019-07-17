571a1a326e92f4ffc7b09bcfc61eb43f32ac4546
zhoujie <994265947@qq.com>
2019-07-17 14:52:31 +0800

    第一次提交

---
+++ Animal.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/7/17
+ * Time: 14:09
+ */
+
+interface Animal
+{
+    public function run();
+    public function study();
+}
